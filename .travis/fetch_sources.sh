d91f17b62f0a4a5325391c801daa347df352842e
Karl Johnson <karljohnson.it@gmail.com>
2020-03-06 14:49:36 -0500

    Travis: Fix sources fetch script.

--- .travis/fetch_sources.sh
+++ .travis/fetch_sources.sh
@@ -2,9 +2,9 @@
 #
 # Fetch nginx-more srpm SOURCES from spectool if URL is provided else from the CDN
 
-spectool -g -C /tmp/nginx-more/SOURCES/ /tmp/nginx-more/SPECS/nginx-more.spec
+spectool -C /tmp/nginx-more/SOURCES/ -g <(rpmspec -P /tmp/nginx-more/SPECS/nginx-more.spec)
 
-for SOURCES in $(spectool -l -S /tmp/nginx-more/SPECS/nginx-more.spec|grep ".tar.gz" | grep -vi 'http:\|https:\|ftp' |awk '{print $2}')
+for SOURCES in $(spectool -l -S <(rpmspec -P /tmp/nginx-more/SPECS/nginx-more.spec)|grep ".tar.gz" | grep -vi 'http:\|https:\|ftp' |awk '{print $2}')
 do
 	wget -q -P /tmp/nginx-more/SOURCES/ "$1""$SOURCES" && echo "$SOURCES downloaded." || echo "Error downloading $SOURCES."
 done
